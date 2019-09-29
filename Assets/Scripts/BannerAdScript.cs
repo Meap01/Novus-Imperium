047c6b8e2912bbd202cc33fc3e9197523bc81238
Daniel Krasovski <dkrasovskijj@gmail.com>
2019-09-29 21:22:07 +0100

    added unity ads

---
+++ Assets/Scripts/BannerAdScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAdScript : MonoBehaviour
+{
+
+    public string gameId = "3308887";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        Advertisement.Banner.Show(placementId);
+    }
+}
